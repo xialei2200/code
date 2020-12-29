ad175c31bfac3a5264db6044a4c037a4c30a65d3
夏磊 <xialei2200@126.com>
2020-12-29 23:07:16 +0800

    初始化

--- redis/src/main/java/vip/xialei/redis/JedisTest.java
+++ redis/src/main/java/vip/xialei/redis/JedisTest.java
@@ -1,6 +1,15 @@
 package vip.xialei.redis;
 
+import redis.clients.jedis.Jedis;
+
 public class JedisTest {
-  public static void main(String[] arg ) {
+  public static void main(String[] arg) {
+  }
+
+  public static void normal() {
+    Jedis jedis = new Jedis("127.0.0.1",6379);
+    jedis.auth("QAZwsx");
+    String value = jedis.get("key");
+    jedis.close();
   }
 }
